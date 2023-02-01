cf53e5bcac4d44e9707645f1fca1f7afae5a329e
Cao Ngọc Sơn <caongocson910@gmail.com>
2023-02-02 05:04:47 +0700

    NS commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,6 @@
 jQuery(document).ready(function ($) {
   "use strict";
   AOS.init();
-  // $(function () {
-  //   $("#tabs").tabs();
-  // });
 
   // Page loading animation
 
@@ -62,36 +59,36 @@ jQuery(document).ready(function ($) {
     });
   }
 
-  // if ($(".owl-clients").length) {
-  //   $(".owl-clients").owlCarousel({
-  //     loop: true,
-  //     nav: false,
-  //     dots: true,
-  //     items: 1,
-  //     margin: 30,
-  //     autoplay: false,
-  //     smartSpeed: 700,
-  //     autoplayTimeout: 6000,
-  //     responsive: {
-  //       0: {
-  //         items: 1,
-  //         margin: 0,
-  //       },
-  //       460: {
-  //         items: 1,
-  //         margin: 0,
-  //       },
-  //       576: {
-  //         items: 3,
-  //         margin: 20,
-  //       },
-  //       992: {
-  //         items: 5,
-  //         margin: 30,
-  //       },
-  //     },
-  //   });
-  // }
+  if ($(".owl-clients").length) {
+    $(".owl-clients").owlCarousel({
+      loop: true,
+      nav: false,
+      dots: true,
+      items: 1,
+      margin: 30,
+      autoplay: false,
+      smartSpeed: 700,
+      autoplayTimeout: 6000,
+      responsive: {
+        0: {
+          items: 1,
+          margin: 0,
+        },
+        460: {
+          items: 1,
+          margin: 0,
+        },
+        576: {
+          items: 3,
+          margin: 20,
+        },
+        992: {
+          items: 5,
+          margin: 30,
+        },
+      },
+    });
+  }
 
   // if ($(".owl-testimonials").length) {
   //   $(".owl-testimonials").owlCarousel({
@@ -153,5 +150,24 @@ jQuery(document).ready(function ($) {
     var $grid = $(".product-list").isotope({
       itemSelector: ".product-grid",
     });
+  });
+
+  $(".accordion > li:eq(0) a").addClass("active").next().slideDown();
+
+  $(".accordion a").click(function (e) {
+    var dropDown = $(this).closest("li").find(".content");
+
+    $(this).closest(".accordion").find(".content").not(dropDown).slideUp();
+
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+    } else {
+      $(this).closest(".accordion").find("a.active").removeClass("active");
+      $(this).addClass("active");
+    }
+
+    dropDown.stop(false, true).slideToggle();
+
+    e.preventDefault();
   });
 });
